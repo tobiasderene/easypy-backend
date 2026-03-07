c6d76a9c13abea9ff9faf91ef73cb5fda19f7b21
KanutoMR <marcelo.rodriguez@upa.edu.py>
2026-03-07 13:40:41 -0300

    security changes

--- app/utils/security.py
+++ app/utils/security.py
@@ -2,10 +2,8 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain[:72], hashed)
